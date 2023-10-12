7647e15c59e50f1e3f6403e7791760c98d023ea7
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-10-12 16:18:00 -0400

    file setup

---
+++ react-restaurant2/app/page.js
@@ -0,0 +1,13 @@
+//main page
+"use client"
+import React from 'react'
+//import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+
+    </main>
+  )
+}

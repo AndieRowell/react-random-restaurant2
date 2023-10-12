75ada09af996fb28dc34316c00ed37e036adaad1
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-10-12 16:28:41 -0400

    adding background image

--- react-restaurant2/app/page.js
+++ react-restaurant2/app/page.js
@@ -2,11 +2,19 @@
 "use client"
 import React from 'react'
 //import Image from 'next/image'
-import styles from './page.module.css'
+//import styles from './page.module.css'
+import background from './img/placeholder.png'
 
+
+//function for main page
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main style={{
+      backgroundImage: `url(/image.png)`,
+      backgroundRepeat: 'no-repeat',
+      }}>
+
+
 
     </main>
   )

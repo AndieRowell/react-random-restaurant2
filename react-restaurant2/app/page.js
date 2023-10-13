05cd6430a727ab7615047ae6c4de8b086dcbf0db
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-10-12 20:58:44 -0400

    trying to setup axios

--- react-restaurant2/app/page.js
+++ react-restaurant2/app/page.js
@@ -1,21 +1,46 @@
 //main page
 "use client"
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 //import Image from 'next/image'
 //import styles from './page.module.css'
-import background from './img/placeholder.png'
+//import background from './img/placeholder.png'  - for background image
+
+const baseURL = "https://www.jsonkeeper.com/b/MDXW";
+
 
 
 //function for main page
 export default function Home() {
-  return (
-    <main style={{
-      backgroundImage: `url(/image.png)`,
-      backgroundRepeat: 'no-repeat',
-      }}>
 
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL)
+      .then((response) => {
+      setData(response.data);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+  }, []);
+
+  if (!data) {
+    // data is loading
+    return <p>Loading...</p>;
+  }
+
+
+  return (
+    <main>
+      //backgroundImage: `url(/image.png)`,
+      //backgroundRepeat: 'no-repeat',
+        <div>
+          <h1>{data.title}</h1>
+          <p>{data.price}</p>
+        </div>
 
 
     </main>
-  )
+  );
 }
